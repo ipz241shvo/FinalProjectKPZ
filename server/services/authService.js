904f048ca90b76a30af9d1d6569258d4e9571b9e
Volodymyr <ipz241_shvo@student.ztu.edu.ua>
2026-05-02 14:27:15 +0300

    feat: add auth service with bcrypt password hashing

---
+++ server/services/authService.js
@@ -0,0 +1,36 @@
+import bcrypt from "bcryptjs";
+import { findUserByLogin, createUser } from "../models/userModel.js";
+
+const SALT_ROUNDS = 10;
+
+export const login = async (login, password) => {
+  const user = await findUserByLogin(login);
+
+  if (!user) {
+    throw new Error("Invalid credentials");
+  }
+
+  const isPasswordValid = await bcrypt.compare(password, user.password);
+
+  if (!isPasswordValid) {
+    throw new Error("Invalid credentials");
+  }
+
+  return user;
+};
+
+export const register = async (login, password) => {
+  const existingUser = await findUserByLogin(login);
+
+  if (existingUser) {
+    throw new Error("Користувач з таким логіном вже існує");
+  }
+
+  if (password.length < 6) {
+    throw new Error("Пароль має бути не менше 6 символів");
+  }
+
+  const hashedPassword = await bcrypt.hash(password, SALT_ROUNDS);
+
+  return await createUser(login, hashedPassword);
+};
